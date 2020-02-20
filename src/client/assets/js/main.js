d819c272724cbd8da37079ee53ee18b1f8524b5f
godoistvan <godopisti@gmail.com>
2020-02-20 20:01:27 +0100

    feat: add darkmode widget
    Add dark mode from npm
    
    #10

--- src/client/assets/js/main.js
+++ src/client/assets/js/main.js
@@ -11,7 +11,24 @@ import 'bootstrap/dist/css/bootstrap.min.css' // Import precompiled Bootstrap cs
 import './../scss/bundle.scss'
 import './../scss/homepage.scss'
 // darkTheme can only be set manually here for now, delete comment below to activate it
-// import './../scss/darkTheme.scss'
-
+import Darkmode from 'darkmode-js';
+var options = {
+    bottom: '64px', // default: '32px'
+    right: 'unset', // default: '32px'
+    left: '32px', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
+  }
+  
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
+new Darkmode().showWidget();
+import './../scss/darkTheme.scss'
 // import FontAwesome icon-pack
 import '@fortawesome/fontawesome-free/css/all.css'
