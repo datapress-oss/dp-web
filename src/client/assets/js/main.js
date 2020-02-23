1d0f1a287724d821d0247c317129369395f98815
Balazs Nyirfa <balazsnyirfa@gmail.com>
2020-02-23 20:00:01 +0100

    refactor: apply standardJS style for main.js

--- src/client/assets/js/main.js
+++ src/client/assets/js/main.js
@@ -11,24 +11,24 @@ import 'bootstrap/dist/css/bootstrap.min.css' // Import precompiled Bootstrap cs
 import './../scss/bundle.scss'
 import './../scss/homepage.scss'
 // darkTheme
-import Darkmode from 'darkmode-js';
-var options = {
-    bottom: '64px', // default: '32px'
-    right: 'unset', // default: '32px'
-    left: '32px', // default: 'unset'
-    time: '0.5s', // default: '0.3s'
-    mixColor: '#fff', // default: '#fff'
-    backgroundColor: '#fff',  // default: '#fff'
-    buttonColorDark: '#100f2c',  // default: '#100f2c'
-    buttonColorLight: '#fff', // default: '#fff'
-    saveInCookies: true, // default: true,
-    label: '🌓', // default: ''
-    autoMatchOsTheme: true // default: true
-  }
-  
-  const darkmode = new Darkmode(options);
-  darkmode.showWidget();
-new Darkmode().showWidget();
+import Darkmode from 'darkmode-js'
 import './../scss/darkTheme.scss'
 // import FontAwesome icon-pack
 import '@fortawesome/fontawesome-free/css/all.css'
+var options = {
+  bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff', // default: '#fff'
+  buttonColorDark: '#100f2c', // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: true, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options)
+darkmode.showWidget()
+new Darkmode().showWidget()
