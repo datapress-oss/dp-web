9c521bc8d47fc088cdfaf8bc2c2a49dcdb059043
Balazs Nyirfa <balazsnyirfa@gmail.com>
2020-03-23 14:38:52 +0100

    fix: fix can not resolve dependency error in darkTheme.scss

--- src/client/assets/js/main.js
+++ src/client/assets/js/main.js
@@ -1,21 +1,17 @@
 // This is the entry file, where all npm modules are imported
-/*
-if (document.getElementbyID("darkSwitch").checked=true) {
 
-    import'./../scss/darkTheme.scss'
-}
-*/
 // import bootstrap javascript bundle (bootstrap.js, jQuary, popper.js)
 import 'bootstrap/dist/js/bootstrap.bundle'
 // Import precompiled Bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 // import custome designs
+
+//default light theme
 import './../scss/bundle.scss'
 import './../scss/homepage.scss'
+//import local DarkTheme.scss
+import './../scss/darkTheme.scss'
 
 // import FontAwesome icon-pack
 import '@fortawesome/fontawesome-free/css/all.css'
-
-//import local DarkTheme.scss
-import './../scss/darkTheme.sccs'
