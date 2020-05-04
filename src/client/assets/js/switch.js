0dcb427aaa978c7ae74daeda50876a530b9bcce3
Hemul <hallerkrisztian1@gmail.com>
2020-05-04 17:14:03 +0200

    improvement: usage of the local storage to save the theme preference
    #27

--- src/client/assets/js/switch.js
+++ src/client/assets/js/switch.js
@@ -1,19 +1,37 @@
 const darkButton = document.getElementById('darkSwitch')
 const darkButtonM = document.getElementById('darkSwitchM')
 const body = document.body
+let darkMode = localStorage.getItem('darkMode'); 
 
 darkButton.onclick = () => {
   if (body.classList.contains('light')) {
     body.classList.replace('light', 'dark')
+    localStorage.setItem('darkMode', 'enabled')
   } else {
     body.classList.replace('dark', 'light')
+    localStorage.setItem('darkMode', null)
+
   }
 }
 
 darkButtonM.onclick = () => {
   if (body.classList.contains('light')) {
     body.classList.replace('light', 'dark')
+    localStorage.setItem('darkMode', 'enabled')
   } else {
     body.classList.replace('dark', 'light')
+    localStorage.setItem('darkMode', null)
   }
+}
+
+
+
+if (darkMode === 'enabled') {
+  body.classList.replace('light', 'dark')
+  document.getElementById("darkSwitch").checked = true;
+
+}else
+{
+  body.classList.replace('dark', 'light')
+  
 }
